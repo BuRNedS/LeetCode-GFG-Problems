2d011e0767d2a0c84c46502f2d89004c3b629e32
Satyam Sundram <satyamsundram360@gmail.com>
2024-10-26 18:01:38 +0530

    Create Occurence of an integer in a Linked List.cpp

---
+++ GFG/Occurence of an integer in a Linked List.cpp
@@ -0,0 +1,9 @@
+int count(struct Node* head, int key) {
+        // add your code here
+        int cnt=0;
+        while(head){
+            if(head->data==key) cnt++;
+            head=head->next;
+        }
+        return cnt;
+    }

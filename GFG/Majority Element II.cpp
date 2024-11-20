3870bd4162b712de2feda1ce5521600e01741bd9
Satyam Sundram <satyamsundram360@gmail.com>
2024-11-20 22:02:21 +0530

    Create Majority Element II.cpp

---
+++ GFG/Majority Element II.cpp
@@ -0,0 +1,13 @@
+vector<int> findMajority(vector<int>& nums) {
+        vector<int>ans;
+        unordered_map<int,int>mp;
+        for(auto &it:nums){
+            mp[it]++;
+        }
+        for(auto it:mp){
+            if(it.second>(nums.size()/3)) ans.push_back(it.first);
+        }
+        sort(ans.begin(),ans.end());
+        if(ans.size()==0) return {-1};
+        return ans;
+    }

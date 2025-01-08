7265be80dc23f9705e3c3827359a163e4e1c6b69
Satyam Sundram <satyamsundram360@gmail.com>
2025-01-08 22:07:32 +0530

    Create Count the number of possible triangles.cpp

---
+++ GFG/Count the number of possible triangles.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    // Function to count the number of possible triangles.
+    int countTriangles(vector<int>& arr) {
+        // code here
+        int n=arr.size();
+        sort(arr.begin(),arr.end());
+        int c=0; //count
+        for(int high=n-1;high>=2;high--)
+        {
+            int low=0,i=high-1;
+            while(low<i)
+            {
+                if(arr[low]+arr[i]>arr[high])
+                {
+                    c+=i-low;
+                    i--;
+                }
+                else
+                    low++;
+            }
+        }
+        return c;
+    }
+};

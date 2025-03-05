1342603e9a51fe7772988abf17b75424082b9d34
Satyam Sundram <satyamsundram360@gmail.com>
2025-03-05 18:55:11 +0530

    Create 5.03.25.cpp

---
+++ GFG/5.03.25.cpp
@@ -0,0 +1,24 @@
+
+class Solution {
+public:
+    int longestStringChain(vector<string>& words) {
+        sort(words.begin(), words.end(), [](const string &a, const string &b) {
+            return a.size() < b.size();
+        });
+
+        unordered_map<string, int> dp;
+        int ans = 0;
+
+        for (string &word : words) {
+            int maxLength = 0;
+            for (int i = 0; i < word.size(); i++) {
+                string prev = word.substr(0, i) + word.substr(i + 1);
+                maxLength = max(maxLength, dp[prev]);
+            }
+            dp[word] = maxLength + 1;
+            ans = max(ans, dp[word]);
+        }
+
+        return ans;
+    }
+};

80c6f772789faf6b5685dbbd96bd9f366535590c
Satyam Sundram <satyamsundram360@gmail.com>
2025-09-11 18:12:19 +0530

    Create 11.09.2025.cpp

---
+++ GFG/11.09.2025.cpp
@@ -0,0 +1,16 @@
+int minJumps(vector<int>& arr) {
+        // code here
+        int jump = 0;
+        int maxJump = 0;
+        int currMax = 0;
+        int n = arr.size();
+        for(int i=0;i<n-1;i++){
+            maxJump = max(maxJump,i+arr[i]);
+            if(currMax == i){
+                currMax = max(currMax,maxJump);
+                jump++;
+            }
+        }
+        if(currMax >= n-1) return jump;
+        return -1;
+    }

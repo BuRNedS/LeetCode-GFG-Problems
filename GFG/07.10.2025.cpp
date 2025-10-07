236dab9a063420f2bb1a422154725222f4b7a65a
Satyam Sundram <satyamsundram360@gmail.com>
2025-10-07 20:12:10 +0530

    Create 07.10.2025.cpp

---
+++ GFG/07.10.2025.cpp
@@ -0,0 +1,44 @@
+class Solution {
+  public:
+    vector<int> bottomView(Node *root) {
+        // code here
+        unordered_map<Node*,int>omap;
+        map<int,vector<Node*>>mapForOrder;
+        
+        queue<Node*>q;
+        q.push(root);
+        omap[root]=0;
+        mapForOrder[0].push_back(root);
+        
+        while(!q.empty()) {
+            int counter=q.size();
+            for(int i=0;i<counter;i++) {
+                Node* vertex=q.front();
+                q.pop(); 
+                int location=omap[vertex];
+                if(vertex->left!=NULL) {
+                    q.push(vertex->left);
+                    omap[vertex->left]=location-1;
+                    mapForOrder[location-1].push_back(vertex->left);
+                }
+                
+                if(vertex->right!=NULL) {
+                    q.push(vertex->right);
+                    omap[vertex->right]=location+1;
+                    mapForOrder[location+1].push_back(vertex->right);
+                }
+            }
+           
+        }
+        
+        
+        vector<int>ans;
+        
+        for(auto i:mapForOrder) {
+            ans.push_back(i.second[i.second.size()-1]->data);
+        }
+        return ans;
+        
+    }
+};
+
